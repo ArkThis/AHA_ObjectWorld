38234e5750527b6a724c13cb305c5db606f5a30a
Peter Bubestinger-Steindl <pb@das-werkstatt.com>
2024-10-08 16:25:16 +0200

    Flask: Added html template

--- code/flask/hello_world/hello.py
+++ code/flask/hello_world/hello.py
@@ -3,12 +3,12 @@
 # https://www.digitalocean.com/community/tutorials/how-to-make-a-web-application-using-flask-in-python-3
 # Thanks!
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello():
-    return 'Hello, World!'
+def index():
+    return render_template('index.html')
 
--- code/flask/hello_world/run.sh
+++ code/flask/hello_world/run.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+# before this, run:
+#  source $dir_to_my_python_virtual_environment/my_venv
+
+OWNDIR=$(dirname $0)
+
 PORT=5000
-export FLASK_APP=hello
+export FLASK_APP=$OWNDIR/hello
 export FLASK_ENV=development
 
 flask run -p $PORT
